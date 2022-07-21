460e06104924402a7f69d46f2cb9f498cf13e45b
yousefalhindawi <eng.yousefalhindawi@gmail.com>
2022-07-21 18:34:39 +0300

    redux

--- 21_07_2022_redux/src/Store/Reducer.js
+++ 21_07_2022_redux/src/Store/Reducer.js
@@ -1,13 +1,7 @@
 export const bankingReducer = (state = 0, action) => {
     switch (action.type) {
         case 'WITHDRAW':  {
-            if(state> 0) {
-                return state - action.payload
-            }
-            else
-            {
-                return state
-            }
+           return state > 0 ? state - action.payload : state
     };
         case 'DIPOSIT': return state + action.payload;
         default: return state;
